8416bb6491d7a9dfd58a96c2282d8ab41b837539
gawd-coder <ansari.ammar786@gmail.com>
2020-05-23 08:16:27 +0530

    Dropping collection

--- test/connection.js
+++ test/connection.js
@@ -6,6 +6,7 @@ mongoose.Promise = global.Promise;
 //now we will tell when the application is complete and when to stop running the code
 //on runs full time, once runs only once
 //connect to the database before tests run
+//done tells when to move on to running the test
 before(function(done){
     mongoose.connection.once("open",function(){
         console.log("Connection has been made");
@@ -13,6 +14,14 @@ before(function(done){
     }).on("error",function(error){
         console.log(`Connection error: ${error}`);
     });
+})
+//emptying the database before every single test so that each test can run in isolation
+//Drop the characters collection before each test
+beforeEach(function(done){
+    //robomongo pluralises our collection name as it assumes each collection to have multiple entries
+    mongoose.connection.collections.mariochars.drop(function(){
+        done();
+    })
 })
 
 
