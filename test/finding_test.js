9822c0fd7f8d3d1af8bf15e33bed49ed3b98c72e
gawd-coder <ansari.ammar786@gmail.com>
2020-05-23 09:53:32 +0530

    finding record

---
+++ test/finding_test.js
@@ -0,0 +1,28 @@
+//not necessary to add though
+const mocha = require("mocha");
+const assert = require("assert");
+const MarioChar = require("../models/mariochar");
+//we have to describe our tests in mocha
+describe("Finding records",function(){
+    //create tests through it blocks. Each it block decribes a single test
+    beforeEach(function(done){
+        //creating new instance for the database
+        var char = new MarioChar({
+            name:"Mario",
+        })
+        //saving the character to database, as we have already connected to testaroo mongoose knows where to put the database. Also the save is an asynchronous request so we use promise to use assert  
+        char.save().then(function(){
+            //isNew returns true if character is made but not saved to database. If its saved so no longer new and hence returns false
+            assert(char.isNew === false);
+            //done parameter to tell when the test is complete and move on
+            done();
+        });
+    })
+    it("Finds one record from the database",function(done){
+        MarioChar.findOne({name: "Mario"}).then(function(result){
+            assert(result.name === "Mario")
+            done();
+        })
+    })
+})
+//to run test using npm test, change the script {test:"mocha"} in package.json file
